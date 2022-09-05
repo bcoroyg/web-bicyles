dae38fe0707bb658b182b48248a89b1b13c560b0
Emilio Coroy <becgonzalez25@gmail.com>
2022-09-05 12:02:51 -0600

    Tarjetas de bicicletas en pagina principal

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import BicycleService from "../services/bicycle.service.js";
 
 const router = Router();
+const bicycleService = BicycleService.getInstance();
 
 router.get('/', async (req, res, next) => {
   try {
+    const bicycles = await bicycleService.getBicycles({});
     res.render('index', {
       title: 'Web Bicicletas',
+      bicycles
     });
   } catch (error) {
     next(error);
